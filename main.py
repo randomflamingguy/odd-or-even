be09059775274716b09544c84637bc4249a63e68
randomflamingguy <randomflamingguy1@gmail.com>
2024-10-03 17:55:14 +0600

    oddeven

---
+++ main.py
@@ -0,0 +1,7 @@
+
+number = int(input("Enter Number to Check"))
+print("Number to be checked :", number)
+if number%2==0:
+    print("This number is an even number")
+else:
+    print("This number is an odd number")
